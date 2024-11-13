dac01fc3842eceec0a7e217f300393d27c249aa9
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:03:29 +0100

    feat: add custom input component

---
+++ src/components/CustomInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import Person from './Person.vue'
+import { defineProps, defineModel } from 'vue';
+
+const props = defineProps(['component', 'value'])
+const model = defineModel()
+</script>
+
+<template>
+    <label>
+        <span>{{ props.component.label }}</span>
+        <input :type="props.component.type" :id="props.component.key" :value="props.value" @change="model = $event.target.value">
+    </label>
+</template>
+
+<style scoped>
+label {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+}
+</style>
