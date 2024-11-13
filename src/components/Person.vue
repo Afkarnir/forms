3871b7c2d4f205ace1a29b1e2ba65a849c7c0b2f
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:03:07 +0100

    feat: add person component

---
+++ src/components/Person.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+
+const props = defineProps(['person'])
+
+const keys = computed(() => Object.keys(props.person))
+const values = computed(() => Object.values(props.person))
+
+const adressEntries = computed(() => Object.entries(props.person.address))
+</script>
+
+<template>
+    <ul v-for="(value, key) in person">
+        <ul v-if="key === 'phones'">
+            <li v-for="data in value">
+                <span>{{ data.type }} :</span>
+                <span>{{ data.number }}</span>
+            </li>
+        </ul>
+
+        <ul v-else-if="key === 'address'">
+            <li v-for="adress in adressEntries">
+                <span>{{ adress[0] }} :</span>
+                <span>{{ adress[1] }}</span>
+            </li>
+        </ul>
+        <li v-else>
+            <span>{{ key }} :</span>
+            <span>{{ value }}</span>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+li {
+    display: flex;
+    gap: 10px;
+}
+</style>
