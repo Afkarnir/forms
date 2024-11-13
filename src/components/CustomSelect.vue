752cdbbeb70b9037a6b6ba198d8c058a7cdedc57
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:28:54 +0100

    fix: fix customselect component model

--- src/components/CustomSelect.vue
+++ src/components/CustomSelect.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import { defineProps, defineModel } from 'vue';
 
-const props = defineProps(['component', 'value'])
+const props = defineProps({
+    component: Object as CustomComponent,
+    value: String
+})
 const model = defineModel()
 </script>
 
 <template>
     <label>
         <span>{{ props.component.label }}</span>
-        <select :id="props.component.key" :value="props.value">
+        <select :id="props.component.key" :value="props.value" @change="model = $event.target.value">
             <option v-for="item in props.component.options.items" :value="item.value">{{ item.label }}</option>
         </select>
     </label>
