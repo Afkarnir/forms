f3a8c88e284daf5518ea170c413e0bb7e83b07cb
Afkarnir <afkarnir@outlook.com>
2024-11-13 20:21:03 +0100

    feat: rework person component + add tableDataValue

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,27 +1,42 @@
 <script setup lang="ts">
-// import { Person } from '../model';
-import { computed } from 'vue';
+import { computed, type PropType } from 'vue';
+import type { DataPerson, Phone } from '../model';
+import { state } from '@/store';
+import TableDataValue from './TableDataValue.vue';
 
-const props = defineProps({
-    person: Object
+defineProps({
+    person: {
+        type: Object as PropType<DataPerson>,
+        required: true,
+    }
 })
+
+const data = computed(() => state.data.data)
 </script>
 
 <template>
     <ul v-for="(value, key) in person">
         <ul v-if="key === 'phones'">
-            <li v-for="data in value">
-                <span>{{ data.type }} :</span>
-                <span>{{ data.number }}</span>
+            <li v-for="phoneData in value as Phone[]">
+                <span>{{ phoneData.type }} :</span>
+                <span>{{ phoneData.number }}</span>
             </li>
         </ul>
 
-        <ul v-else-if="key === 'address'">
-            <li v-for="(value, key) in person.address">
-                <span>{{ key }} :</span>
-                <span>{{ value }}</span>
-            </li>
-        </ul>
+        <template v-else-if="key === 'address'">
+            <span>{{ key }} :</span>
+            <ul>
+                <li v-for="(value, key) in person.address">
+                    <span>{{ key }} :</span>
+                    <span>{{ value }}</span>
+                </li>
+            </ul>
+        </template>
+
+        <template v-else-if="key === 'projects' || key === 'notes'">
+            <TableDataValue :table="value as string[]" :label="key" />
+        </template>
+        
         <li v-else>
             <span>{{ key }} :</span>
             <span>{{ value }}</span>
