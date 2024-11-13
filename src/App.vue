139ab151142c97946044e5c647089093be892047
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:04:08 +0100

    feat: add routing

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
+  <header>
+    <router-link :to="{ name: 'formulaire-1' }">formulaire 1</router-link>
+    <router-link :to="{ name: 'formulaire-2' }">formulaire 2</router-link>
+    <router-link :to="{ name: 'formulaire-3' }">formulaire 3</router-link>
+  </header>
   <RouterView />
 </template>
 
 <style scoped>
+header {
+  position: fixed;
+  top: 0;
+  padding: 16px;
 
+  display: flex;
+  gap: 16px;
+}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FirstForm from '../views/firstForm/FirstForm.vue'
+import FirstForm from '../views/firstForm/firstForm.vue'
+import SecondForm from '../views/secondForm/secondForm.vue'
+import ThirdForm from '../views/thirdForm/thirdForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: '/formulaire-1',
+      name: 'formulaire-1',
       component: FirstForm,
     },
+    {
+      path: '/formulaire-2',
+      name: 'formulaire-2',
+      component: SecondForm,
+    },
+    {
+      path: '/formulaire-3',
+      name: 'formulaire-3',
+      component: ThirdForm,
+    },
+    {
+      path: '/**',
+      redirect: '/formulaire-1',
+    }
   ],
 })
 
