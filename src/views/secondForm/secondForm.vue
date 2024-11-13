f42c04d6382ca162f4392a97f89f2e12e1f3eb71
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:04:50 +0100

    feat: add form 2 + form 3 skeleton

---
+++ src/views/secondForm/secondForm.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { getData } from '../../data.ts'
+import { ref, reactive, onMounted } from 'vue'
+
+const data = reactive({})
+
+onMounted(() => {
+    getData('json2.json').then(res => {
+        Object.assign(data, res)
+    })
+})
+</script>
+
+<template>
+    <h1>formulaire 2</h1>
+</template>
+
+<style scoped>
+</style>
