dbd95a298b8d38303bc33ed09fa7029f4af34211
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:03:41 +0100

    feat: add custom select component

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps, defineModel } from 'vue';
+
+const props = defineProps(['component', 'value'])
+const model = defineModel()
+</script>
+
+<template>
+    <label>
+        <span>{{ props.component.label }}</span>
+        <select :id="props.component.key" :value="props.value">
+            <option v-for="item in props.component.options.items" :value="item.value">{{ item.label }}</option>
+        </select>
+    </label>
+</template>
