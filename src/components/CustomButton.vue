748f5208f04bf2f1b876b1df726457a8533af24a
Afkarnir <afkarnir@outlook.com>
2024-11-13 15:03:19 +0100

    feat: add custom buttton component

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps(['label', 'keyValue'])
+</script>
+
+<template>
+    <button type="button" :id="props.keyValue">{{ props.label }}</button>
+</template>
+
+<style scoped>
+</style>
